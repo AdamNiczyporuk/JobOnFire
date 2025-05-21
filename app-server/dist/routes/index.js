83bbc7df0657cb5a9621d30e389b05f05b57ba86
FireFly0707 <jakubswietliczny@gmail.com>
2025-05-21 20:27:59 +0200

    delete dist

--- app-server/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const auth_1 = require("./auth");
-exports.router = (0, express_1.Router)();
-exports.router.use("/auth", auth_1.router);
